02e3040ffbb747b7abe683d28290e156f3350afa
harinishpradeeep-web <harinishpradeeep@gmail.com>
2026-09-15 11:49:54 +0530

    Fix: changed env redeploy

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,7 @@ interface ScheduleRow extends ScheduledEmail {
 
 
 
+
 export default function DashboardPage() {
   const { me } = useSession();
   const [schedules, setSchedules] = useState<ScheduleRow[] | null>(null);
